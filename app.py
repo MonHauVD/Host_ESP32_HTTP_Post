eb38ff612912067fcac28c001f4925659fcbee8b
MonHauVD <tranvietdung121@gmail.com>
2024-12-25 16:27:26 +0700

    flask ver

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from http.server import SimpleHTTPRequestHandler
-import os
+from flask import Flask, request, send_file, jsonify
+import threading
+import time
 import sys
-from flask import Flask, request, jsonify
-
+import logging
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-    handler = MyServer(request.environ, request.environ['wsgi.errors'], request.environ['wsgi.input'])
-    return handler.do_GET()
-
-hostName = "192.168.0.102"
-# hostName = "127.0.0.1"
-# hostName = "localhost"
-serverPort = 8080
 
 class DualStream:
     def __init__(self, terminal, log_file):
         self.terminal = terminal
         self.log_file = log_file
+        self.suppress_terminal = False
 
     def write(self, message):
         self.terminal.write(message)
@@ -31,83 +21,82 @@ class DualStream:
 
 # Open log file
 log_file = open('server.log', 'w')
-sys.stdout = DualStream(sys.stdout, log_file)
+dual_stream = DualStream(sys.stdout, log_file)
+sys.stdout = dual_stream
+sys.stderr = dual_stream
 
-class MyServer(SimpleHTTPRequestHandler):
-    def log_message(self, format, *args):
-        # if self.path == '/':
-        #     return
-        if self.path == '/log':
-            return
-        super().log_message(format, *args)
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            with open('server.log', 'r') as file:
-                log_content = file.read()
-            response = f"""
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Hello World</title>
-                <script>
-                    function fetchLog() {{
-                        fetch('/log')
-                            .then(response => response.text())
-                            .then(data => {{
-                                document.getElementById('log').innerText = data;
-                            }});
-                    }}
-                    setInterval(fetchLog, 1000); // Refresh every second
-                </script>
-            </head>
-            <body>
-                <h1>Hello World</h1>
-                <pre id="log">{log_content}</pre>
-            </body>
-            </html>
-            """
-            self.wfile.write(bytes(response, "utf-8"))
-        elif self.path == '/log':
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            with open('server.log', 'r') as file:
-                log_content = file.read()
-            self.wfile.write(bytes(log_content, "utf-8"))
-        else:
-            return SimpleHTTPRequestHandler.do_GET(self)
-    def do_POST(self):
-        # content_length = 10
-        # try:
-        content_length = int(self.headers['Content-Length'])
-        # except:
-        #     pass
-        body = self.rfile.read(content_length)
-        print("Body: ", body.decode('utf-8'))
-        log_file.flush() 
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        message = "-1"
-        if(body.decode('utf-8') == "123"):
-            message = "Success 1"
-        else:
-            message = "Failed 1"
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
-        self.wfile.write(bytes(message, "utf-8"))
+@app.before_request
+def before_request():
+    if request.path == '/log':
+        dual_stream.suppress_terminal = True
+
+# @app.after_request
+# def after_request(response):
+#     dual_stream.suppress_terminal = False
+#     return response
+
+# Serve the main HTML page
+@app.route('/')
+def index():
+    with open('server.log', 'r') as file:
+        log_content = file.read()
+    response = f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hello World</title>
+        <script>
+            function fetchLog() {{
+                fetch('/log')
+                    .then(response => response.text())
+                    .then(data => {{
+                        document.getElementById('log').innerText = data;
+                    }});
+            }}
+            setInterval(fetchLog, 1000); // Refresh every second
+        </script>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        <pre id="log">{log_content}</pre>
+    </body>
+    </html>
+    """
+    return response
+
+# Serve the log file as plain text
+@app.route('/log')
+def log():
+    return send_file('server.log', mimetype='text/plain')
+
+# Handle POST requests
+@app.route('/process', methods=['POST'])
+def process():
+    body = request.data.decode('utf-8')
+    print("Body: ", body)
+    log_file.flush()
+    response_message = "Failed 1"
+    if body == "123":
+        response_message = "Success 1"
+    return response_message
+
+# Background logging thread
+# def log_writer():
+#     with open('server.log', 'w') as log_file:
+#         while True:
+#             log_file.write(f"Server running at {time.ctime()}\n")
+#             log_file.flush()
+#             time.sleep(1)
 
 if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    webServer.server_close()
-    print("Server stopped.")
-    log_file.close()
+    # Start a logging thread
+    # logging_thread = threading.Thread(target=log_writer, daemon=True)
+    # logging_thread.start()
+
+    # Run the Flask server
+    app.run(host='192.168.0.102', port=8080)
